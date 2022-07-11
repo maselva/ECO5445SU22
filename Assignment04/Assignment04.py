502415ec51fc54242cfdaea6736a709d2251550e
maselva <maselva@knights.ucf.edu>
2022-07-10 20:54:28 -0400

    Create Assignment04.py

---
+++ Assignment04/Assignment04.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 10 20:18:02 2022
+
+@author: mselv
+"""
+
+import numpy as np
+
+n = 1000000
+
+#Random data points across the square
+data = np.random.uniform(-0.5, 0.5, size=(n, 2))
+
+#Count points that are within 0.5 from center
+inside = len(np.argwhere(np.linalg.norm(data, axis=1) < 0.5))
+
+#Calculate pi
+print(inside / n * 4)
