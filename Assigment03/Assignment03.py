9d87f4ae3b9aaaa3e2b92f0a66fa604adb3bc424
JoshuaEubanksUCF <joshua.eubanks@ucf.edu>
2022-07-13 14:34:41 -0400

    Graded assignment 3

--- Assigment03/Assignment03.py
+++ Assigment03/Assignment03.py
@@ -42,24 +42,30 @@ A[1:4, 2:4]
 Number 4
 """
 
-B = np.array([[2.*A-8.]])
-
+B = np.array([[2.*A-8.]]) # Created a triple layered array (-2)
+B = 2.*A-8.
 #I
 
 np.sum(B, axis=3)
-
+np.sum(B, axis=1)
 #II
 
 np.sum(B, axis=2)
-
+np.sum(B, axis=0)
 #III
 
-row_totals = B.sum()
+
+row_totals = B.sum() # Missing cummulative sum. Used sum instead (-5)
+row_totals = np.sum(np.cumsum(B, axis=1),axis = 1)
+
 print("Cumulative Sum of Row Values:", row_totals)
+
 
 #IV
 
-column_totals = B.sum()
+column_totals = B.sum() # Missing cummulative sum. Used sum instead (-5)
+# The cumulative sum of column values
+column_totals = np.sum(np.cumsum(B, axis=0),axis = 0)
 print("Cumulative Sum of Column Values:", column_totals)
 
 """
@@ -90,4 +96,4 @@ M = np.array([[1., 20.],[1., -40.]])
 
 N = np.array([[286.],[88.]])
 
-np.dot(np.linalg.inv(M), N)
+np.dot(np.linalg.inv(M), N) # Which is the equilibrium price and which is quantity? (-4)
