c21f1e34cef3ff71b100e7f27ead4a7f459bb72a
JoshuaEubanksUCF <joshua.eubanks@ucf.edu>
2022-07-13 13:12:53 -0400

    Graded assignment

--- Assignment04/Assignment04.py
+++ Assignment04/Assignment04.py
@@ -13,7 +13,7 @@ n = 1000000
 data = np.random.uniform(-0.5, 0.5, size=(n, 2))
 
 #Count points that are within 0.5 from center
-inside = len(np.argwhere(np.linalg.norm(data, axis=1) < 0.5))
+inside = len(np.argwhere(np.linalg.norm(data, axis=1) <= 0.5)) # Boundary needed to be included (-10)
 
 #Calculate pi
 print(inside / n * 4)
