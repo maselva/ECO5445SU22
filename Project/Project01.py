86ef35e3d827f26301a8c44807b0ed026fce23c8
maselva <maselva@knights.ucf.edu>
2022-07-17 17:30:43 -0400

    Update Project01.py

--- Project/Project01.py
+++ Project/Project01.py
@@ -7,6 +7,8 @@ Created on Sat Jul 16 09:39:29 2022
 
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Find out the current directory.
 os.getcwd()
@@ -18,4 +20,108 @@ os.getcwd()
 
 
 mortgage_dataset = pd.read_csv("hmda_sw.csv", delimiter=',')
+
+mortgage_dataset.rename(columns={'s13':'Applicant_race'},inplace=True)
+mortgage_dataset.rename(columns={'s46':'D_to_I_Ratio_Total_Obligations'},inplace=True)
+mortgage_dataset.rename(columns={'s45':'D_to_I_Ratio_Housing_Expenses'},inplace=True)
+mortgage_dataset.rename(columns={'s27a':'Self-employed'},inplace=True)
+mortgage_dataset.rename(columns={'s23a':'Marital_status'},inplace=True)
+mortgage_dataset.rename(columns={'school':'Years_of_Education'},inplace=True)
+mortgage_dataset.rename(columns={'s30a':'Applicant_Monthly_Employment_Income'},inplace=True)
+mortgage_dataset.rename(columns={'s30c':'Coapplicant_Monthly_Employment_Income'},inplace=True)
+mortgage_dataset.rename(columns={'s7':'Action_Taken'},inplace=True)
+"""
+Creating a new Dataframe which contains
+the variables we want to use in our analysis
+"""
+newData = mortgage_dataset[['Applicant_race','D_to_I_Ratio_Total_Obligations','D_to_I_Ratio_Housing_Expenses','Self-employed','Marital_status','Years_of_Education','Applicant_Monthly_Employment_Income','Action_Taken']]
+newData.describe()
+
+"""
+Gathering summary statistics on different variables in order to 
+see how a typical applicant would fall into these categories
+"""
+
+Applicant_Race = np.array(newData["Applicant_race"])
+
+White_Applicants = np.count_nonzero(Applicant_Race == 5)
+print(((White_Applicants/2380)*100),'percent of applicants are white')
+
+DebttoIncome_Ratio = np.array(newData["D_to_I_Ratio_Total_Obligations"])
+
+DtoI_Histogram = plt.hist(DebttoIncome_Ratio,bins=[12,20,28,36,44,52,60,68,76,84])
+print(((DtoI_Histogram[0][2]/2380)*100),'percent of applicants have a Debt to Income Ratio that falls between 28 and 36')
+
+Self_emp = np.array(newData["Self-employed"])
+
+Self_Employed_Applicants = np.count_nonzero(Self_emp == 1)
+print(((Self_Employed_Applicants/2380)*100),'percent of applicants are self-employed')
+
+Married = np.array(newData["Marital_status"])
+
+Married_Applicants = np.count_nonzero(Married == 'M')
+print(((Married_Applicants/2380)*100),'percent of applicants are married')
+"""
+Creating a scatterplot to show the correlation between years of education
+and monthly income
+"""
+
+Yrs_of_Education = np.array(newData["Years_of_Education"])
+Monthly_Income = np.array(newData["Applicant_Monthly_Employment_Income"])
+
+plt.xlim(0,40)
+plt.scatter(Yrs_of_Education,Monthly_Income)
+"""
+Using the scatterplot for reference, you can see that the beginning
+of the larger increases in monthly revenue correlate to surpassing 13 years of
+education, meaning the individual pursued educational beyond high school.
+"""
+
+#Explanation of composition of sample and representative applicant
+
+"""
+As can be seen with the count functions used above, the vast majority 
+of these applicants were white (2041 out of 2380). Most were also married 
+and employed by others. Therefore, a representative applicant would be a white, 
+married individual who has a great chance of being approved for his mortgage.
+"""
+
+#Probability of approval
+
+Loan_Status = np.array(newData["Action_Taken"])
+
+Loan_Originated = np.count_nonzero(Loan_Status == 1)
+Approved_not_executed = np.count_nonzero(Loan_Status == 2)
+Approved_Applicants = Loan_Originated + Approved_not_executed
+
+Probability_of_Approval = ((Approved_Applicants/2380)*100)
+print("The baseline probabilty of an individual being approved is", 
+      Probability_of_Approval,"percent")
+"""
+Table for Number 6
+"""
+
+blackDenied = newData.loc[(newData['Applicant_race'] == 3) & (newData['Action_Taken'] == 3)]
+blackDenied.describe() #count is 96
+
+whiteDenied = newData.loc[(newData['Applicant_race'] == 5) & (newData['Action_Taken'] == 3)]
+whiteDenied.describe() #count is 189
+
+"""
+Using the above calculations, you can fill out the remaining 
+portions of the table.
+"""
+
+Approval_table = np.array([['Black',243,96,339],['White',1852,189,2041],['Total',2095,285,2380]])
+print(Approval_table)
+
+"""
+Number 7
+"""
+
+P_Approved_White = (((White_Applicants/2380)*(Approved_Applicants/2380))/
+                    (White_Applicants/2380))
+
+P_Denied_Black = (((1-(White_Applicants/2380))*(1-(Approved_Applicants/2380)))/
+                  (1-(White_Applicants/2380)))
 
